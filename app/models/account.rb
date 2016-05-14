16b60a4a129fd54a1bd3aac9c531b7f29cea7b1b
tchiao <tiffanya.chiao@gmail.com>
2016-05-14 13:03:17 -0700

    Add defined relationships

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,7 @@
 class Account < ActiveRecord::Base
+  belongs_to :organization
+  has_many :emails
+  has_many :members, through: :organization
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,6 @@
 class Member < ActiveRecord::Base
+  belongs_to :organization
+  has_many :emails
+  has_one :address
+
 end
